0b9017de277892dccdc5584a2a0957fc63c600df
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-11-23 01:24:16 +0300

    wip

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -8,24 +8,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Tuğran Demirel" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/admin/images/favicon.ico">
+    <link rel="shortcut icon" href="{{ asset('assets/admin/images/favicon.ico') }}">
 
     <!-- jsvectormap css -->
-    <link href="assets/admin/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet" type="text/css" />
 
     <!--Swiper slider css-->
-    <link href="assets/admin/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/libs/swiper/swiper-bundle.min.css') }}" rel="stylesheet" type="text/css" />
 
     <!-- Layout config Js -->
-    <script src="assets/admin/js/layout.js"></script>
+    <script src="{{ asset('assets/admin/js/layout.js') }}"></script>
     <!-- Bootstrap Css -->
-    <link href="assets/admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="assets/admin/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="assets/admin/css/app.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- custom Css-->
-    <link href="assets/admin/css/custom.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
 
 </head>
 
@@ -862,27 +862,27 @@
 </div>
 
 <!-- JAVASCRIPT -->
-<script src="assets/admin/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/admin/libs/simplebar/simplebar.min.js"></script>
-<script src="assets/admin/libs/node-waves/waves.min.js"></script>
-<script src="assets/admin/libs/feather-icons/feather.min.js"></script>
-<script src="assets/admin/js/pages/plugins/lord-icon-2.1.0.js"></script>
-<script src="assets/admin/js/plugins.js"></script>
+<script src="{{ asset('assets/admin/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/admin/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/admin/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('assets/admin/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+<script src="{{ asset('assets/admin/js/plugins.js') }}"></script>
 
 <!-- apexcharts -->
-<script src="assets/admin/libs/apexcharts/apexcharts.min.js"></script>
+<script src="{{ asset('assets/admin/libs/apexcharts/apexcharts.min.js') }}"></script>
 
 <!-- Vector map-->
-<script src="assets/admin/libs/jsvectormap/js/jsvectormap.min.js"></script>
-<script src="assets/admin/libs/jsvectormap/maps/world-merc.js"></script>
+<script src="{{ asset('assets/admin/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
+<script src="{{ asset('assets/admin/libs/jsvectormap/maps/world-merc.js') }}"></script>
 
 <!--Swiper slider js-->
-<script src="assets/admin/libs/swiper/swiper-bundle.min.js"></script>
+<script src="{{ asset('assets/admin/libs/swiper/swiper-bundle.min.js') }}"></script>
 
 <!-- Dashboard init -->
-<script src="assets/admin/js/pages/dashboard-ecommerce.init.js"></script>
+<script src="{{ asset('assets/admin/js/pages/dashboard-ecommerce.init.js') }}"></script>
 
 <!-- App js -->
-<script src="assets/admin/js/app.js"></script>
+<script src="{{ asset('assets/admin/js/app.js') }}"></script>
 </body>
 </html>
